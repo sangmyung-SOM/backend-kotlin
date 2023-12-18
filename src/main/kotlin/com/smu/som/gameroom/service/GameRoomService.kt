381025ec4f23d4eb2ec06c78257d7d13e7118485
Jung So Young <soyoung5611@gmail.com>
2023-12-18 22:41:03 +0900

    style: 방 삭제 요청시 오류 확인 print 삽입

--- src/main/kotlin/com/smu/som/gameroom/service/GameRoomService.kt
+++ src/main/kotlin/com/smu/som/gameroom/service/GameRoomService.kt
@@ -51,6 +51,9 @@ class GameRoomService (var gameRooms: HashMap<String, GameRoomSetting> = LinkedH
 		if (findById(roomId) != null) {
 			gameRooms.remove(roomId)
 		}
+		else {
+			println("No such room $roomId")
+		}
 	}
 	fun updateState(roomId: String, b: Boolean) : Boolean {
 		if (findById(roomId) != null) {
