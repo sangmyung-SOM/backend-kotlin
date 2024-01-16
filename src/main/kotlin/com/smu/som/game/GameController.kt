7bf81487c345e6eaf7cb938ad58a9dd5d0669ce1
Jung So Young <soyoung5611@gmail.com>
2024-01-17 01:57:00 +0900

    GameController 수정

--- src/main/kotlin/com/smu/som/game/GameController.kt
+++ src/main/kotlin/com/smu/som/game/GameController.kt
@@ -2,24 +2,37 @@ package com.smu.som.game
 
 import com.smu.som.chatwithroom.ChatRoom
 import com.smu.som.chatwithroom.ChatService
+import com.smu.som.reportQnA.ReadReportDTO
+import com.smu.som.reportQnA.ReportService
 import lombok.RequiredArgsConstructor
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.*
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/game")
-class GameController {
+@RequestMapping("/api/game")
+class GameController(
+	private val reportService: ReportService
+) {
 
-	var gameService: GameService = GameService()
-	var chatService: ChatService = ChatService()
-//
-//	@GetMapping("/room/{roomId}")
-//	@ResponseBody
-//	fun roomInfo(@PathVariable roomId: String): ChatRoom?{
-//		return chatService.findById(roomId)
-//	}
+	@PostMapping("/{roomId}/qna")
+	@ResponseBody
+	fun sendQnA(
+		@PathVariable(name = "roomId") roomId: String,
+		@RequestBody qna: ReadReportDTO
+	) : ResponseEntity<Any> {
+		println("sendQnA answer : ${qna.answer}")
+		return ResponseEntity.ok().body(reportService.sendQnA(roomId, qna))
+	}
+
+	@GetMapping("/{roomId}/qna")
+	@ResponseBody
+	fun getQnA(
+		@PathVariable(name = "roomId") roomId: String
+	) : List<ReadReportDTO> {
+		println("getQnA")
+		return reportService.getQnA(roomId)
+	}
+
 }
