3874f50d72a4c875f9a51ef2c8beb5807677fbf3
Jung So Young <soyoung5611@gmail.com>
2024-01-17 01:48:23 +0900

    GameController 주석 처리

--- src/main/kotlin/com/smu/som/game/GameController.kt
+++ src/main/kotlin/com/smu/som/game/GameController.kt
@@ -3,8 +3,6 @@ package com.smu.som.game
 import com.smu.som.chatwithroom.ChatRoom
 import com.smu.som.chatwithroom.ChatService
 import lombok.RequiredArgsConstructor
-import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.simp.SimpMessageSendingOperations
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,10 +16,10 @@ class GameController {
 
 	var gameService: GameService = GameService()
 	var chatService: ChatService = ChatService()
-
-	@GetMapping("/room/{roomId}")
-	@ResponseBody
-	fun roomInfo(@PathVariable roomId: String): ChatRoom?{
-		return chatService.findById(roomId)
-	}
+//
+//	@GetMapping("/room/{roomId}")
+//	@ResponseBody
+//	fun roomInfo(@PathVariable roomId: String): ChatRoom?{
+//		return chatService.findById(roomId)
+//	}
 }
